c414ced4df81c6b1e09460cdbdc389374d0f0189
maximecornaton <maxime.cornaton@cpe.fr>
2023-12-11 23:59:15 +0100

    Notification

--- NotificationService/src/main/java/com/notifapp/notificationservice/controller/NotificationController.java
+++ NotificationService/src/main/java/com/notifapp/notificationservice/controller/NotificationController.java
@@ -4,12 +4,20 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import com.notifapp.notificationservice.service.NotificationService;
+
 @Controller
 public class NotificationController {
 
-    @MessageMapping("/notify")
-    @SendTo("/topic/notification")
-    public String sendNotification(String message) {
-        return message;
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @MessageMapping("/inscription")
+    @SendTo("/topic/confirmationInscription")
+    public String confirmationInscription(String message) {
+        return notificationService.processInscription(message);
     }
 }
--- NotificationService/src/main/java/com/notifapp/notificationservice/service/NotificationService.java
+++ NotificationService/src/main/java/com/notifapp/notificationservice/service/NotificationService.java
@@ -1,20 +1,14 @@
 package com.notifapp.notificationservice.service;
 
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
 
-    private final SimpMessagingTemplate messagingTemplate;
-
-    public NotificationService(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
-    }
-
-    public void sendNotification(String content) {
-        NotificationMessage message = new NotificationMessage();
-        message.setContent(content);
-        messagingTemplate.convertAndSend("/topic/notification", message);
+    public String processInscription(String message) {
+        // Effectuer le traitement d'inscription
+        
+        // Renvoyer un message de confirmation
+        return "Inscription confirmée!";
     }
 }
